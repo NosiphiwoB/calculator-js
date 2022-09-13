aca990c8c94cb1409e95c9c7d8c1bf367500f8b9
NosiphiwoB <biyelanosiphiwo@gmail.com>
2022-09-13 13:44:07 +0200

    calculator

---
+++ app.js
@@ -0,0 +1,77 @@
+// const btns = document.getElementById("btns")
+
+function btn1(){
+
+	document.case.display.value += "1"
+};
+function btn2(){
+
+	document.case.display.value += "2"
+};
+function btn3(){
+
+	document.case.display.value += "3"
+};
+function btn4(){
+
+	document.case.display.value += "4"
+};
+function btn5(){
+
+	document.case.display.value += "5"
+};
+
+function btn6(){
+
+	document.case.display.value += "6"
+};
+
+function btn7(){
+
+	document.case.display.value += "7"
+};
+
+function btn8(){
+
+	document.case.display.value += "8"
+};
+
+function btn9(){
+
+	document.case.display.value += "9"
+};
+
+function btn0(){
+
+	document.case.display.value += "0"
+};
+function plusBtn(){
+
+	document.case.display.value += "+"
+};
+function minusBtn(){
+
+	document.case.display.value += "-"
+};
+function divideBtn(){
+
+	document.case.display.value += "/"
+};
+function multiplyBtn(){
+
+	document.case.display.value += "*"
+};
+
+function clearBtn(){
+
+	document.case.display.value = ""
+};
+
+function decimalBtn(){
+    document.case.display.value += "." 
+}
+
+function equalsBtn() {
+	var equals = eval(document.case.display.value)
+	document.case.display.value = equals;
+}
