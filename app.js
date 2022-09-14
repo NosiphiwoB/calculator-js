f746c92df45e1ab9325d2ffaa07b63ab2a389f0f
NosiphiwoB <biyelanosiphiwo@gmail.com>
2022-09-14 20:42:59 +0200

    added javascript

--- app.js
+++ app.js
@@ -1,12 +1,46 @@
-const display = document.getElementById("display");
+let display = document.getElementById("display");
 
 let buttons = Array.from(document.getElementsByClassName("button"));
 
 buttons.map(button => {
-	button.addEventListener('click', (e) => {
+	button.addEventListener("click", (e) => {
 		switch(e.target.innerText){
+			case "AC":
+				display.innerText = " ";
+				break;
+				case "C":
+					display.innerText = display.innerText.slice(0, -1);
+					break;
+            case "=":
+				try{
+					display.innerText = eval(display.innerText);
+
+				}catch{
+					display.innerText = 'Invalid!';
+				}
+				 break;
+
 			default:
-				display.innerText += e.target.innerText;
+				display.innerText += e.target.innerText
 		}
-	});
-});
+	})
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
